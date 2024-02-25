7df9683d50a52163466d6fe92257bc96afb8a2d3
JustinKeeJoonHong <105581488+JustinKeeJoonHong@users.noreply.github.com>
2024-02-24 16:47:13 -0800

    change url from auth0 -> fsndjk.us

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
   auth0: {
-    url: "fsndjk.us.auth0.com", // the auth0 domain prefix
+    url: "fsndjk.us", // the auth0 domain prefix
     audience: "coffee", // the audience set for the auth0 app
     clientId: "JFBJjduQfmf0zPzknE0xkG5FpaWb1RdJ", // the client id generated for the auth0 app
-    callbackURL: "http://localhost:8100", // the base url of the running ionic application.
+    callbackURL: "https://127.0.0.1:8100/login-results", // the base url of the running ionic application.
   },
 };
